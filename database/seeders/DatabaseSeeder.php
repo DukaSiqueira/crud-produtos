fd2b53892619fd226752c8d233f2f9d98181f9d7
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2023-06-23 13:51:44 -0300

    FEAT: Adicionado Seeder
    Seeders para gerar dados de utilização

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UserSeeder::class);
+        $this->createUser('Liberfly User', 'libuser@example.com', 'liberfly123');
+        $this->createUser('Liberfly Test', 'libtest@example.com', '1liber2fly3');
+
+        $this->createTask('Trabalho faculdade', 'Trabalho de conclusão de curso');
+        $this->createTask('Janta amigos', 'Jantar com amigos as 22:00');
+        $this->createTask('Remédio', 'Tomar remédio as 12:00');
+        $this->createTask('Alongamento', 'Alongamento depois de acordar');
+        $this->createTask('Estudos', 'Estudar para a prova de matemática');
+    }
+
+    private function createUser(string $name, string $email, string $password): void
+    {
+        User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+        ]);
+    }
+
+    private function createTask(string $title, string $description): void
+    {
+        Task::create([
+            'title' => $title,
+            'description' => $description,
+        ]);
     }
 }
